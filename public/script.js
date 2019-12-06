acee6edb6c4cd6e56e79a361942b97ad778bdc2b
SnorkelwackJr <38191711+SnorkelwackJr@users.noreply.github.com>
2019-12-06 13:11:53 -0700

    First commit

---
+++ public/script.js
@@ -0,0 +1,84 @@
+var app = new Vue({
+  el: '#root',
+  data: {
+    items: [],
+    text: "",
+    author: "",
+    selected: {}
+  },
+  methods: {
+    color(item) {
+      if (item.upvotes >= 0) {
+        return "color: blue";
+      }
+      else {
+        return "color: red";
+      }
+    },
+    async getItems() {
+      try {
+        let response = await axios.get("/api/items");
+        this.items = response.data.reverse();
+        return true;
+      }
+      catch (error) {
+        console.log(error);
+      }
+    },
+    async add() {
+      try {
+        let r = await axios.post('/api/items', {
+          author: this.author,
+          text: this.text
+        });
+        this.getItems();
+        this.author = "";
+        this.text = "";
+      }
+      catch (error) {
+        console.log(error);
+      }
+    },
+    async post() {
+      try {
+        let response = await axios.put("/api/post", {
+          author: this.selected.author,
+          text: this.selected.text
+        });
+        return true;
+      }
+      catch (error) {
+        console.log(error);
+      }
+    },
+    async upVote(item) {
+      try {
+        let response = await axios.put("/api/upvote", {
+          author: item.author,
+          text: item.text
+        });
+        this.getItems();
+        return true;
+      }
+      catch (error) {
+        console.log(error);
+      }
+    },
+    async downVote(item) {
+      try {
+        let response = await axios.put("/api/downvote", {
+          author: item.author,
+          text: item.text
+        });
+        this.getItems();
+        return true;
+      }
+      catch (error) {
+        console.log(error);
+      }
+    }
+  },
+  created() {
+    this.getItems();
+  }
+});
---
+++ server.js
@@ -0,0 +1,98 @@
+const express = require('express');
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+app.use(express.static('public'));
+
+const mongoose = require('mongoose');
+
+// connect to the database
+mongoose.connect('mongodb://localhost:27017/status', {
+    useNewUrlParser: true
+});
+
+const itemSchema = new mongoose.Schema({
+    author: String,
+    upvotes: Number,
+    text: String
+});
+
+const Item = mongoose.model('Item', itemSchema);
+
+app.post('/api/items', async(req, res) => {
+    const item = new Item({
+        author: req.body.author,
+        text: req.body.text,
+        upvotes: 0
+    });
+    try {
+        await item.save();
+        res.send(item);
+    }
+    catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+});
+
+app.get('/api/items', async(req, res) => {
+    try {
+        let items = await Item.find();
+        res.send(items);
+    }
+    catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+});
+
+app.put('/api/post', async(req, res) => {
+    try {
+        let items = req.body.items;
+        for (let i = 0; i < items.length; i++) {
+            let item = await Item.findOne({ author: items[i].author, text: items[i].text });
+            item.upvotes++;
+            await item.save();
+        }
+        res.send(items);
+    }
+    catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+});
+
+app.put('/api/upvote', async(req, res) => {
+    try {
+        let item = await Item.findOne({ author: req.body.author, text: req.body.text });
+        item.upvotes = item.upvotes + 1;
+        await item.save();
+        res.send(item);
+    }
+    catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+});
+
+app.put('/api/downvote', async(req, res) => {
+    try {
+        let item = await Item.findOne({ author: req.body.author, text: req.body.text });
+        item.upvotes = item.upvotes - 1;
+        await item.save();
+        res.send(item);
+    }
+    catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+});
+
+app.listen(3000, () => console.log('Server listening on port 3000!'));
+
+module.exports = app;
